a1e56db49d5cacea0ea30797f003b97a84d937e7
abdulhost <tempm029@gmail.com>
2025-02-22 01:05:01 +0530

    class done

--- assets/classes.php
+++
@@ -1,6 +0,0 @@
-<?php 
-require_once 'search-class-table.php';
-?>
-<div>
-    <?php render_class_table('class'); ?>
-</div>
--- assets/delete-class.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (isset($_GET['id'])) {
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'class_sections';
-    $id = intval($_GET['id']);
-    $wpdb->delete($table_name, array('id' => $id));
-    echo "<div class='updated'><p>Class and sections deleted successfully!</p></div>";
-}
-?>
